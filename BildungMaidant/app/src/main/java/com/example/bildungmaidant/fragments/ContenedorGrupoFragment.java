5eb66d71fe7f50cb38551efeab682cf5eba7cfd9
TonatiuhCruz9 <crat200199@gmail.com>
2020-05-20 02:32:07 -0500

    Agregando Fragments en Fragment Contenedor_Grupo Parte 2 LISTO

--- BildungMaidant/app/src/main/java/com/example/bildungmaidant/fragments/ContenedorGrupoFragment.java
+++ BildungMaidant/app/src/main/java/com/example/bildungmaidant/fragments/ContenedorGrupoFragment.java
@@ -21,24 +21,25 @@ import java.util.ArrayList;
 public class ContenedorGrupoFragment extends Fragment {
     private ViewPager viewPager;
     private TabLayout tabLayout;
-    private Toolbar toolbar;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_contenedor_grupo,container,false);
-
         viewPager=v.findViewById(R.id.fcgViewPager);
         tabLayout=v.findViewById(R.id.fcgTabLayout);
-        toolbar=v.findViewById(R.id.fcgToolbar);
         setUpViewPager();
-
         return v;
     }
 
     private ArrayList<Fragment> agregarFragments(){
         ArrayList<Fragment> fragments = new ArrayList<>();
         fragments.add(new GrupoFragment());
+        fragments.add(new RecordatoriosTareasFragment());
+        fragments.add(new MensajeNuevoFragment());
+        fragments.add(new RecursosDidacticosFragment());
+        fragments.add(new MiembrosFragment());
+        fragments.add(new AvisosFragment());
         return fragments;
     }
 
@@ -47,5 +48,10 @@ public class ContenedorGrupoFragment extends Fragment {
         tabLayout.setupWithViewPager(viewPager);
 
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_menu_grupo_grupo_24);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_menu_grupo_recordatorios_tareas_24);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_menu_grupo_mensaje_24);
+        tabLayout.getTabAt(3).setIcon(R.drawable.ic_menu_grupo_recursos_didacticos_24);
+        tabLayout.getTabAt(4).setIcon(R.drawable.ic_menu_grupo_miembros_24);
+        tabLayout.getTabAt(5).setIcon(R.drawable.ic_menu_grupo_avisos_24);
     }
 }
